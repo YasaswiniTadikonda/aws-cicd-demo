f42d3442169077bd57baa56d00aa8f06fdeb7bf4
Yasaswini Tadikonda <yasaswinitadikonda@gmail.com>
2026-07-16 11:50:32 +0530

    Test automatic deployment

---
+++ js/script.js
@@ -0,0 +1,1 @@
+console.log("SkillBridge Loaded Successfully!");
--- script.js
+++
@@ -1,4 +0,0 @@
-function showMessage() {
-    document.getElementById("message").innerHTML =
-        "🎉 Deployment Successful! Welcome to AWS CI/CD using GitHub Actions.";
-}
