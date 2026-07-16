b8feefea173c6e3e35971d4f95a5465d1d8cfc9a
Yasaswini Tadikonda <yasaswinitadikonda@gmail.com>
2026-07-16 10:34:25 +0530

    Added GitHub Actions workflow

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function showMessage(){
-    alert("Deployment Successful!");
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "🎉 Deployment Successful! Welcome to AWS CI/CD using GitHub Actions.";
 }
